c7af0ca920627463db9f733084c829cd592e2cee
Troy <tdreuther@gmail.com>
2023-11-04 13:46:59 -0400

    adding files

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import { SanityClient } from "@sanity/client";
+
+export default SanityClient({
+    projectId: "eb0pdclv",
+    dataset: "production",
+});
---
+++ studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'eb0pdclv',
+    dataset: 'production'
+  }
+})
